8643a4bfcd3fe378e8935780ea0298e13a1ec18c
Kevin Damiani <kevin.damiani@diva-e.com>
2022-12-02 15:34:51 +0100

    add features add partials html; add swiper and example; add postcss and autoprefixer;

--- src/js/default.js
+++ src/js/default.js
@@ -1,1 +1,30 @@
-console.log('is worked')
+import $ from 'jquery'
+import Swiper from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+const obj = {
+  a: "Apple",
+  b: "Peanut Butter"
+}
+console.log(obj, obj.c?.fa);
